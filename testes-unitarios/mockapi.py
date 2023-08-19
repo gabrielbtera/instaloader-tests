735b4fee6d01f7f279d23bad2913355d40ae6492
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2023-08-19 16:37:33 -0300

    test: testes de integracao da classe post

---
+++ testes-unitarios/mockapi.py
@@ -0,0 +1,45 @@
+def mock_api ():
+    
+    return {"shortcode":"CsJv4hNJzdb", 
+        'edge_sidecar_to_children': {"edges": [
+            {"node": {"__typename":"GraphImage",
+               "id":"3101220405636326522", "is_video": False, }},
+            {"node": {"__typename":"GraphImage",
+               "id":"3101220405636326522", "is_video": True, }}
+            ]
+        }
+         ,"__typename":"GraphSidecar", 'edge_media_to_comment': {'count': 19690}, 
+         "owner":{
+                "id":"25025320",
+                "username":"instagram"
+                },
+
+        "edge_media_to_caption":{
+            "edges":[
+                {
+                    "node":{
+                    "text":"comentario da materia de testes @professorGlauco #testesIntegracao #ufs"
+                    }
+                }
+            ]},
+        "edge_media_to_tagged_user":{
+            "edges":[
+                {
+                    "node":{
+                    "user":{
+                        "full_name":"Ludo ⤫ mountaindog ⤫ foxred labrador",
+                        "followed_by_viewer":False,
+                        "id":"36540927655",
+                        "is_verified":False,
+                        "profile_pic_url":"https://instagram.faju4-2.fna.",
+                        "username":"gabrielSilva"
+                    },
+                    "x":0.0,
+                    "y":0.0
+                    }
+                }
+            ]
+   }, "video_view_count": 3000,
+      'is_video': True
+
+        }
--- testes-unitarios/testes-integracao.py
+++ testes-unitarios/testes-integracao.py
@@ -1,13 +1,67 @@
-
-
 import unittest
+from unittest import mock
 
+import instaloader
+
+from instaloader.instaloader import Instaloader
+from instaloader.structures import Hashtag, Post
+
+from .mockapi import mock_api
+
+L = instaloader.Instaloader()
+posts = instaloader.Profile.from_username(L.context, "instagram").get_posts()
+
+# for post in posts:
+#     print(post.caption_hashtags)
+
+# print(posts)
+
+mockapi =  mock_api()
 
 # execute python3 -m testes-unitarios.testes-integracao
 
+
 class IntegrationTests(unittest.TestCase):
-    def test_hello_world(self):
-        self.assertEqual('hello', 'hello')
+    context = mock.Mock(Instaloader())
+    def test_media_is_downloadable_video(self):
+        context = mock.Mock(Instaloader())
+        post = Post(context.context, mockapi)
+        videos = [False, True]
+        self.assertEquals(videos, post.get_is_videos())
+       
+    
+    def test_get_user_downloadable_profile(self):
+        context = mock.Mock(Instaloader())
+        post = Post(context.context, mockapi)
+        self.assertEqual(post.owner_username, 'instagram' )
+        self.assertEqual(post.owner_id, '25025320')
+    
+
+    def test_downloadable_caption(self):
+        context = mock.Mock(Instaloader())
+        post = Post(context.context, mockapi)
+        self.assertEqual(post.caption, "comentario da materia de testes @professorGlauco #testesIntegracao #ufs")
+        self.assertEquals(post.tagged_users, ["gabrielsilva"])
+        self.assertEquals(post.caption_hashtags, ['testesintegracao', 'ufs'])
+        self.assertEquals(post.caption_mentions, ['professorglauco'])
+
+
+    def test_count_views_and_media_interations(self):
+        context = mock.Mock(Instaloader())
+        post = Post(context.context, mockapi)
+        self.assertEqual(post.video_view_count, 3000)
+        self.assertEqual(post.mediacount, 2)
+        self.assertEqual(post.comments, 19690)
+
+    
+    
+
+    
+
+
+
+
+    
 
 
 if __name__== '__main__':
